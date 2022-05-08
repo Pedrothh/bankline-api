8068e6709573b1631e751422f73c08ad339fc6a5
Pedro Thomé <pedro.th.dev@gmail.com>
2022-05-08 17:02:42 -0300

    web sv rodando porta localhost:8080/correntista

---
+++ src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -0,0 +1,23 @@
+package com.dio.santander.bankline.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dio.santander.bankline.api.model.Correntista;
+import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+
+@RestController
+@RequestMapping("/correntistas")
+public class CorrentistaController {
+	@Autowired
+	private CorrentistaRepository repository;
+	
+	@GetMapping
+	public List<Correntista> findAll(){	
+		return repository.findAll();
+	}
+}
--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -26,6 +26,10 @@ public class Movimentacao {
 	
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
+	
+	@Column(name = "id_conta")
+	private Integer idConta;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -55,6 +59,12 @@ public class Movimentacao {
 	}
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
+	}
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
 	}
 	
 }
