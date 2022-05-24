ef933aacc68097cebbdf870e3d20d09b9064f4fa
Pedro Thomé <pedro.th.dev@gmail.com>
2022-05-24 10:47:37 -0300

    Correção de bugs no application.properties

--- src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -8,7 +8,6 @@ public class BanklineApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
-		//iremos começar a codificar o nosso projeto
 	}
 
 }
